072bd7b0ce9fbaf6efe182db03d6539cd7303165
unknown <hagydaniel@gmail.com>
2020-01-04 18:03:25 -0600

    update

--- src/components/TitleDrawer/PageTitle.vue
+++ src/components/TitleDrawer/PageTitle.vue
@@ -1,16 +1,24 @@
 <template>
-  <v-container>
-    <v-col cols="12">
-      <v-row :align="center" :justify="justify">
-        <h1>Eventhub Dashboard Title Container</h1>
-      </v-row>
-    </v-col>
-  </v-container>
+  <v-jumbotron color="grey lighten-4">
+    <v-container>
+      <v-col cols="12">
+        <v-row :align="center" :justify="justify">
+          <v-layout align-center>
+            <v-flex>
+              <h3 class="display-3">Eventhub Dashboard</h3>
+              <v-divider class="my-3"></v-divider>
+              <v-btn large color="primary" class="mx-0">Select Company</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-row>
+      </v-col>
+      </v-container>
+  </v-jumbotron>
 </template>
 
 <script>
 export default {
-    data() {
+  data() {
     return {
       alignment: "center",
       justify: "center"
